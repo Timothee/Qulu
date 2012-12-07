486979d48b63d598a8e29895984b12aa4686708f
Timothée Boucher <timothee@gmail.com>
2012-12-06 21:02:28 -0800

    Add play icon on hover

--- popup.js
+++ popup.js
@@ -8,7 +8,7 @@ window.onload = function () {
 			var shows = JSON.parse(localStorage["Qulu:shows"]);
 			var html = "<ul>";
 			for (var i = 0; i < shows.length; i++) {
-				html += "<li><a href='http://www.hulu.com/watch/" + shows[i].id + "' target='_BLANK'><img src='" + shows[i]["thumbnail_url"] + "'/><span>" + shows[i]["title"] + "</a></li>";
+				html += "<li><a href='http://www.hulu.com/watch/" + shows[i].id + "' target='_BLANK'><img class='thumbnail' src='" + shows[i]["thumbnail_url"] + "'/><img class='play' src='images/play.png'/><span>" + shows[i]["title"] + "</a></li>";
 			}
 			html += "</ul>";
 			list.innerHTML = html;
