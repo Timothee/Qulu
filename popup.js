90b816a403bae37cdaceee4a8e56b1c1e140b78b
Timothée Boucher <timothee@gmail.com>
2012-12-08 09:46:57 -0800

    Clean up style for empty queue and logged out

--- popup.js
+++ popup.js
@@ -1,21 +1,26 @@
 window.onload = function () {
-	var list = document.getElementById("list");
+	var container = document.getElementById("container");
+	var logged_out = document.getElementById("logged_out");
+	var empty_queue = document.getElementById("empty_queue");
+	var queue = document.getElementById("queue");
+
 	if (localStorage["Qulu:loggedIn"] == "false") {
-		list.innerHTML = "You are not logged in";
-		list.className = "loggedout";
+		container.className = "logged_out";
+		queue.innerHTML = "You are not logged in";
+		queue.className = "loggedout";
 	} else {
 		if (parseInt(localStorage["Qulu:queueLength"])) {
+			container.className = "queue";
 			var shows = JSON.parse(localStorage["Qulu:shows"]);
 			var html = "<ul>";
 			for (var i = 0; i < shows.length; i++) {
 				html += "<li><a href='http://www.hulu.com/watch/" + shows[i].id + "' target='_BLANK'><img class='thumbnail' src='" + shows[i]["thumbnail_url"] + "'/><img class='play' src='images/play.png'/><span>" + shows[i]["title"] + "</a></li>";
 			}
 			html += "</ul>";
-			list.innerHTML = html;
+			queue.innerHTML = html;
 			console.log(shows);
 		} else {
-			list.innerHTML = "Your queue is empty.";
-			list.className = "empty";
+			container.className = "empty_queue";
 		}
 	}
 };
