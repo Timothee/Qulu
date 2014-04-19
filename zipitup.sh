984c24a4b99a942976de538c82f16a7bd4b30b44
Timothée Boucher <timothee@gmail.com>
2014-04-19 15:48:05 -0700

    Update zip script

--- zipitup.sh
+++ zipitup.sh
@@ -2,6 +2,19 @@
 cd src/
 rm ../Qulu.zip
 cp js/mixpanel.prod.js js/mixpanel.js
-zip -r ../Qulu.zip js/background.js js/popup.js js/mixpanel.js js/lib/*.js css/* images/*.png manifest.json html/*
+zip -r ../Qulu.zip\
+    js/lib/jquery-2.1.0.min.js\
+    js/lib/underscore-min.js\
+    js/lib/backbone-min.js\
+    js/lib/backbone.localStorage.js\
+    js/background.js\
+    js/popup.js\
+    js/mixpanel.js\
+    js/Episode.js\
+    js/Queue.js\
+    css/*\
+    images/*.png\
+    manifest.json\
+    html/*
 cp js/mixpanel.dev.js js/mixpanel.js
 cd -
